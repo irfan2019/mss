742b3153185f885235ef099e4216e0f07cfa66c4
irfan2019 <48289347+irfan2019@users.noreply.github.com>
2022-06-27 12:28:06 +0530

    successful

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -4,34 +4,45 @@ class AddContact extends React.Component {
   state = {
     name: "",
     email: "",
+    msg:"",
   };
 
   add = (e) => {
     e.preventDefault();
-    if (this.state.name === "" || this.state.email === "") {
+    if (this.state.name === "" || this.state.email === "" || this.state.m_no===""||this.state.msg === ""||this.state.address==="" ) {
       alert("ALl the fields are mandatory!");
       return;
     }
     this.props.addContactHandler(this.state);
-    this.setState({ name: "", email: "" });
+    this.setState({ name: "", email: "",m_no:"",address:"",msg:"" });
   };
   render() {
     return (
       <div className="ui main">
-        <h2>Add Contact</h2>
+        <h2>Add user details</h2>
         <form className="ui form" onSubmit={this.add}>
           <div className="field">
-            <label>Name</label>
             <input
               type="text"
               name="name"
               placeholder="Name"
+              maxLength={25}
               value={this.state.name}
               onChange={(e) => this.setState({ name: e.target.value })}
             />
           </div>
           <div className="field">
-            <label>Email</label>
+            
+            <input
+              type="text"
+              name="m_no"
+              placeholder="Mobile-Number"
+              value={this.state.m_no}
+              onChange={(e) => this.setState({ m_no: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            
             <input
               type="text"
               name="email"
@@ -40,9 +51,33 @@ class AddContact extends React.Component {
               onChange={(e) => this.setState({ email: e.target.value })}
             />
           </div>
-          <button className="ui button blue">Add</button>
+          <div className="field">
+            
+            <input
+              type="text"
+              name="addr"
+              placeholder="Address"
+              value={this.state.address}
+              onChange={(e) => this.setState({ address: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            
+            <input
+              type="text"
+              name="email"
+              
+              placeholder="Type of Assistance"
+              
+              value={this.state.msg}
+              onChange={(e) => this.setState({ msg: e.target.value })}
+            />
+          </div>
+          <button className="ui button yellow">Send  Message</button>
         </form>
       </div>
+      
+
     );
   }
 }
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,13 +2,16 @@ import React from "react";
 import user from "../images/user.png";
 
 const ContactCard = (props) => {
-  const { id, name, email } = props.contact;
+  const { id, name, email,msg,m_no,address } = props.contact;
   return (
     <div className="item">
       <img className="ui avatar image" src={user} alt="user" />
       <div className="content">
-        <div className="header">{name}</div>
-        <div>{email}</div>
+        <div className="header"><label>name of the user: </label>{name}</div>
+        <div><label>mobile_no: </label>{m_no}</div>
+        <div><label>Email of the user: </label>{email}</div>
+        <div><label>Address of the user:</label>{address}</div>
+        <div><label>Type of assistance: </label>{msg}</div>
       </div>
       <i
         className="trash alternate outline icon"
